80efc8c21bf5178e86cd927aec8bbf851d890564
Nur Ilham Iskandar <nur.ilham18@gmail.com>
2018-12-28 14:26:09 +0700

    first commit

---
+++ build/precache-manifest.47a663e832d86a7368160a0ccd57b3cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "019e5d0dd5bbebe28d7f67e551395012",
+    "url": "/static/media/logo-6.019e5d0d.png"
+  },
+  {
+    "revision": "3f21cb77887fd7d277a6c42853a81125",
+    "url": "/static/media/image.3f21cb77.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bc9fba52364c00730118",
+    "url": "/static/js/main.bc9fba52.chunk.js"
+  },
+  {
+    "revision": "df0fe4e9f637b643460d",
+    "url": "/static/js/1.df0fe4e9.chunk.js"
+  },
+  {
+    "revision": "bc9fba52364c00730118",
+    "url": "/static/css/main.06232f2f.chunk.css"
+  },
+  {
+    "revision": "1328978ee6f57261f9123ab2eb0da39e",
+    "url": "/index.html"
+  }
+];
